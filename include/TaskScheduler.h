a68fe75c969d8d9b9876214c66ebbc9b6c866ff0
Dloped <diego.lttwobrothers@gmail.com>
2026-08-27 10:54:34 +0200

    arreglado archivo

--- include/TaskScheduler.h
+++ include/TaskScheduler.h
@@ -1,20 +1,29 @@
-#ifndef KRYPT_TASK_SCHEDULER_H
-#define KRYPT_TASK_SCHEDULER_H
+#ifndef KRYPT_KERNEL_TASK_SCHEDULER_H
+#define KRYPT_KERNEL_TASK_SCHEDULER_H
 
 #include <Arduino.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <freertos/semphr.h>
+
+// -----------------------------------------------------------------------------
+// Solución al conflicto con la HAL de Arduino:
+// Arduino.h define macros globales '#define LOW 0x0' y '#define HIGH 0x1'.
+// Se desdefinen temporalmente para no romper la declaración del enum TaskPriority.
+// -----------------------------------------------------------------------------
+#ifdef LOW
+  #undef LOW
+#endif
+
+#ifdef HIGH
+  #undef HIGH
+#endif
 
 namespace Krypton {
 
     enum class TaskPriority : UBaseType_t {
         IDLE = 0,
         LOW = 1,
-        NORMAL = 2,
+        MEDIUM = 2,
         HIGH = 3,
-        CRITICAL = 4,
-        REALTIME = 5
+        CRITICAL = 4
     };
 
     class TaskScheduler {
@@ -27,9 +36,13 @@ namespace Krypton {
 
         bool init();
 
-        // Asigna tareas directamente a PRO_CPU (Core 0) o APP_CPU (Core 1)
-        bool createTask(TaskFunction_t taskFunc, const char* name, uint32_t stackSize, 
-                        void* param, TaskPriority priority, uint8_t coreID, TaskHandle_t* outHandle = nullptr);
+        bool createTask(TaskFunction_t taskFunc,
+                        const char* name,
+                        uint32_t stackDepth,
+                        void* param,
+                        TaskPriority priority,
+                        uint8_t coreID,
+                        TaskHandle_t* outHandle = nullptr);
 
         void suspendTask(TaskHandle_t handle);
         void resumeTask(TaskHandle_t handle);
@@ -40,4 +53,13 @@ namespace Krypton {
 
 } // namespace Krypton
 
-#endif // KRYPT_TASK_SCHEDULER_H
+// Opcional: Si los usuarios del Kernel necesitan las macros de Arduino GPIO 
+// más adelante en el mismo archivo, se pueden redefinir al final:
+#ifndef LOW
+  #define LOW 0x0
+#endif
+#ifndef HIGH
+  #define HIGH 0x1
+#endif
+
+#endif // KRYPT_KERNEL_TASK_SCHEDULER_H
